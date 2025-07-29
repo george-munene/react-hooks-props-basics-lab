b9a732475ee418c2a8a02492e9f91455701fdd11
George Munene <ngangaredeemed@gmail.com>
2025-07-29 07:49:25 +0300

    demonstrates use of components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from './Links';
 
-function About() {
+function About(prop) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+       {prop.bio && prop.bio.length > 0 ? <p>{prop.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github = {prop.links.github} linkedin = {prop.links.linkedin}/>
     </div>
   );
 }
